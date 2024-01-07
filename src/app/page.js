94605ab10636917eb68bd6614ccb50d6ab1ba458
virgil <wilson.westbrook.personal@gmail.coom>
2024-01-06 17:38:04 -0800

    Prints Server Start Time on Home Page
    next.config.js remembers start time. StartUpTime.js uses it.

--- src/app/page.js
+++ src/app/page.js
@@ -6,9 +6,11 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
+        
         <p>
-          <StartUpTime />
+         <StartUpTime /> <b>( Virgil Remix )</b>
         </p>
+        
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
--- src/components/StartUpTime.js
+++ src/components/StartUpTime.js
@@ -1,28 +1,11 @@
-"use client"; // This is a client component
-import React, { useEffect, useState } from 'react';
+"use client";
+import React from 'react';
+
+const dateString = process.env.SERVER_START_TIME;
+const startTime = new Date(dateString).toLocaleTimeString();
 
 const StartUpTime = () => {
-  const [startupTime, setStartupTime] = useState('...');
-
-  useEffect(() => {
-    const fetchStartupTime = async () => {
-      try {
-        const response = await fetch('/api/ServerStartTime');
-        if (!response.ok) {
-          throw new Error('Failed to fetch startup time');
-        }
-        const data = await response.json();
-        setStartupTime(data.startupTime);
-      } catch (error) {
-        console.error(error);
-        setStartupTime('???');
-      }
-    };
-
-    fetchStartupTime();
-  }, []);
-
-  return <span>Startup Time: {startupTime}</span>;
+  return <span>Startup Time: {startTime}</span>;
 };
 
 export default StartUpTime;
